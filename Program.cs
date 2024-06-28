fe4e425b35ab66806bb3082f047a489d913acb85
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-06-28 08:57:06 +0500

    added feature calculated age differense 0.0.1 version

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Hello please Type your name : ");
+string userName = Console.ReadLine();
+Console.Write($"Hello {userName} plaese type your age : ");
+short userAge = System.Convert.ToInt16(Console.ReadLine());
+short keschasAge = 2;
+
+Console.WriteLine($"Difference your and keschas age {userAge - keschasAge}");
